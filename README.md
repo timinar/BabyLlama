86740f76afc70e6fb76cafa47d41e97de75caf1d
timinar <timinar@gmail.com>
2023-08-10 12:52:31 +0200

    add training codes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BabyLlama
 Training code for Baby-Llama, our submission to the strict-small track of the BabyLM challenge.
+
+We perform some basic regex-based cleaning of the dataset and then train a tokenizer on the cleaned dataset. This is performed in `cleaning_and_tokenization.ipynb`. The notebook assumes that the babylm dataset (`/babylm_10M` and `/babylm_dev`) is placed or symlinked in the `/data` folder.
--- babylm_dataset.py
+++ babylm_dataset.py
@@ -1,9 +1,8 @@
 import os
 import torch
-from torch.utils.data import Dataset, Subset
-from random import sample, randrange
+from torch.utils.data import Dataset
+from random import randrange
 from pathlib import Path
-# from transformers import GPT2TokenizerFast
 
 class BabylmDataset(Dataset):
     """
@@ -12,16 +11,18 @@ class BabylmDataset(Dataset):
     tokenizer.bos_token = "<s>"
     tokenizer.eos_token = "</s>"
     tokenizer.pad_token = "<pad>"
-    train_dataset = BabylmDataset(PATH / "data/babylm_10M", SEQ_LENGTH, tokenizer=tokenizer, offset=0)
+    train_dataset = BabylmDataset(PATH / "data/babylm_10M", SEQ_LENGTH, tokenizer=tokenizer, random_chunk=True)
     full_eval_dataset = BabylmDataset(PATH / "data/babylm_dev", SEQ_LENGTH, tokenizer=tokenizer, offset=0)
-
     eval_indices = sample(range(len(full_eval_dataset)), EVAL_SAMPLES)
     eval_dataset = Subset(full_eval_dataset, eval_indices)
-
     """
-    def __init__(self, data_dir: str, seq_length: int, tokenizer):
+
+    def __init__(self, data_dir: str, seq_length: int, tokenizer, offset: int=0, random_chunk: bool=False):
         self.seq_length = seq_length
+        self.offset = offset
         self.tokenizer = tokenizer
+        self.random_chunk = random_chunk
+
         tokenizer_name = tokenizer.__class__.__name__
         tokenized_file = Path(os.path.join(data_dir, f"tokenized_{tokenizer_name}_{tokenizer.vocab_size}.pt"))
 
@@ -46,9 +47,14 @@ class BabylmDataset(Dataset):
             torch.save(self.data, tokenized_file)
 
     def __len__(self):
-        return len(self.data) // self.seq_length - 1
+        if self.random_chunk:
+            return len(self.data) // self.seq_length - 1
+        else:
+            return (len(self.data) - self.offset) // self.seq_length
 
     def __getitem__(self, i):
-        offset = randrange(self.seq_length) # Sample random offset between 0 and seq_length-1
-        return self.data[i*self.seq_length+offset:(i+1)*self.seq_length+offset]
-
+        if self.random_chunk:
+            offset = randrange(self.seq_length) # Sample random offset between 0 and seq_length-1
+            return self.data[i*self.seq_length+offset:(i+1)*self.seq_length+offset]
+        else:
+            return self.data[i*self.seq_length+self.offset:(i+1)*self.seq_length+self.offset]
